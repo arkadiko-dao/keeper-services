bfb3bdd1e789719352b54d421f5a6df3892c2553
Philip De Smedt <philip.desmedt@gmail.com>
2021-10-24 20:35:19 +0200

    add script to write DIKO prices json to S3

--- scrape-diko-price.js
+++ scrape-diko-price.js
@@ -5,54 +5,55 @@ const tx = require('@stacks/transactions');
 const fs = require('fs');
 const utils = require('./utils');
 const network = utils.resolveNetwork();
+const AWS = require('aws-sdk');
+const BUCKET_NAME = 'arkadiko-prices';
+const axios = require('axios');
 
-if (!fs.existsSync('prices.json')) {
-  fs.writeFile('prices.json', JSON.stringify({}), (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("File written successfully");
-    }
+const fetchPair = async () => {
+  let details = await tx.callReadOnlyFunction({
+    contractAddress: CONTRACT_ADDRESS,
+    contractName: "arkadiko-swap-v1-1",
+    functionName: "get-pair-details",
+    functionArgs: [
+      tx.contractPrincipalCV(CONTRACT_ADDRESS, 'arkadiko-token'),
+      tx.contractPrincipalCV(CONTRACT_ADDRESS, 'usda-token')
+    ],
+    senderAddress: CONTRACT_ADDRESS,
+    network: network,
   });
-} else {
-  fs.readFile('prices.json', async (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      const fetchPair = async () => {
-        let details = await tx.callReadOnlyFunction({
-          contractAddress: CONTRACT_ADDRESS,
-          contractName: "arkadiko-swap-v1-1",
-          functionName: "get-pair-details",
-          functionArgs: [
-            tx.contractPrincipalCV(CONTRACT_ADDRESS, 'arkadiko-token'),
-            tx.contractPrincipalCV(CONTRACT_ADDRESS, 'usda-token')
-          ],
-          senderAddress: CONTRACT_ADDRESS,
-          network: network,
-        });
 
-        return tx.cvToJSON(details);
-      };
+  return tx.cvToJSON(details);
+};
 
-      const pair = await fetchPair();
-      if (pair.success) {
-        const pairDetails = pair.value.value.value;
-        const dikoPrice = (pairDetails['balance-y'].value / pairDetails['balance-x'].value).toFixed(4);
-        const timestamp = Date.now();
+(async () => {
+  const pair = await fetchPair();
+  if (pair.success) {
+    const pairDetails = pair.value.value.value;
+    const dikoPrice = (pairDetails['balance-y'].value / pairDetails['balance-x'].value).toFixed(4);
+    const timestamp = Date.now();
 
-        obj = JSON.parse(data);
-        obj[timestamp] = {};
-        obj[timestamp]['diko-usda'] = dikoPrice;
-        json = JSON.stringify(obj);
-        fs.writeFile('prices.json', json, (err) => {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log("File written successfully");
-          }
-        });
+    const s3 = new AWS.S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET
+    });
+
+    const url = 'https://arkadiko-prices.s3.eu-central-1.amazonaws.com/prices.json';
+    const response = await axios.get(url);
+    let obj = response.data;
+    obj[timestamp] = {};
+    obj[timestamp]['diko-usda'] = dikoPrice;
+    json = JSON.stringify(obj);
+
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: 'prices.json',
+      Body: json
+    };
+    s3.upload(params, function(err, data) {
+      if (err) {
+        throw err;
       }
-    }
-  });
-}
+      console.log(`File uploaded successfully. ${data.Location}`);
+    });
+  }
+})();
