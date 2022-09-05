aa6a0d08f2ee0167467d1a881d2aa07c28c41ad4
philipdesmedt <philip.desmedt@gmail.com>
2022-09-05 15:51:46 +0200

    fix: atALEX price diff

--- update-price.js
+++ update-price.js
@@ -206,7 +206,7 @@ const getPrices = async () => {
             const atAlexPrice = res2['laplace_current_token_price']['0']['avg_price_usd'];
             const atAlexName = res2['laplace_current_token_price']['0']['token'];
             getPrice('auto-alex').then(async (prevAtAlexPrice) => {
-              prevAtAlexPrice = prevAtAlexPrice / 1000000;
+              prevAtAlexPrice = prevAtAlexPrice / 100000000;
               const alexDiff = atAlexPrice / prevAtAlexPrice;
               if (atAlexName === 'auto-alex' && alexDiff < 1.3 && alexDiff > 0.7) {
                 console.log('publishing new STX price', stxPrice, 'and BTC price', btcPrice, 'and atALEX price', atAlexPrice);
